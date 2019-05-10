08c5ab4ee4b4fc834914326dde82316fdc258701
jianye <jianye@cloud.com>
2019-05-10 16:18:34 +0800

    +list_files

--- list_files.php
+++ list_files.php
@@ -25,3 +25,26 @@
       南无阿弥陀佛🙏
       心中有佛🙏
  */
+
+/**
+ * 获取某个目录下文件路径
+ * 
+ * [list_files description]
+ * @param  [type] $dir [description]
+ * @return [type]      [description]
+ */
+function list_files($dir) {
+    if (is_dir($dir)) {
+        if ($handle = opendir($dir)) {
+            while (($file = readdir($handle)) !== FALSE) {
+                if ($file != "." && $file != ".." && $file != "Thumbs.db") {
+                    echo '<a target="_blank" href="' . $dir . $file . '">' . $file . '</a><br />' . "
+";
+                }
+            }
+            closedir($handle);
+        }
+    }
+}
+
+list_files('./');
